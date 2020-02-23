c13095109082c638483470f36dea3425e94d7b35
NocturnalProgress <26844071+NocturnalProgress@users.noreply.github.com>
2020-02-22 22:34:38 -0500

    Created new TicTacToe

--- GUI/TicTacToe/TicTacToe.java
+++ GUI/TicTacToe/TicTacToe.java
@@ -1,4 +1,4 @@
-package TicTacToe;
+package GUI.TicTacToe;
 
 import java.awt.*;
 import java.awt.event.*;
@@ -6,6 +6,26 @@ import java.awt.event.*;
 import javax.swing.*;
 
 class TicTacToe {
+    static int square1 = 0;
+    static int square2 = 0;
+    static int square3 = 0;
+    static int square4 = 0;
+    static int square5 = 0;
+    static int square6 = 0;
+    static int square7 = 0;
+    static int square8 = 0;
+    static int square9 = 0;
+
+    static boolean b1clickable = true;
+    static boolean b2clickable = true;
+    static boolean b3clickable = true;
+    static boolean b4clickable = true;
+    static boolean b5clickable = true;
+    static boolean b6clickable = true;
+    static boolean b7clickable = true;
+    static boolean b8clickable = true;
+    static boolean b9clickable = true;
+
     public static void main(String[] args) {
         JFrame f = new JFrame();
 
@@ -19,59 +39,77 @@ class TicTacToe {
         JButton b8 = new JButton("8");
         JButton b9 = new JButton("9");
 
+        if (square1 == 1) {
+
+        } else if (square2 == 1) {
+
+        }
+
         b1.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent a) {
-
+                if (b1clickable = true) {
+                    square1++;
+                    b1.setText("X");
+                }
             }
         });
 
         b2.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent a) {
-
+                square2++;
+                b2.setText("X");
             }
         });
 
         b3.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent a) {
-
+                square3++;
+                b3.setText("X");
             }
         });
 
         b4.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent a) {
-
+                square4++;
+                b4.setText("X");
             }
         });
 
         b5.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent a) {
-
+                square5++;
+                b5.setText("X");
             }
         });
 
         b6.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent a) {
-
+                square6++;
+                b6.setText("X");
             }
         });
 
         b7.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent a) {
-
+                square7++;
+                b7.setText("X");
             }
         });
 
         b8.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent a) {
-
+                square8++;
+                b8.setText("X");
             }
         });
 
         b9.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent a) {
-
+                square9++;
+                b9.setText("X");
             }
         });
+
         f.add(b1);
         f.add(b2);
         f.add(b3);
