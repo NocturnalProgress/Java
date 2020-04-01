fa64ed424a1851fd26351106ebfdb42c3049d874
NocturnalProgress <26844071+NocturnalProgress@users.noreply.github.com>
2020-04-01 14:21:02 -0400

    Created shop menu

---
+++ GUI/ClickerGame/ShopMenu.java
@@ -0,0 +1,48 @@
+package GUI.ClickerGame;
+
+import javax.swing.*;
+import javax.swing.border.*;
+import java.awt.event.*;
+import java.awt.*;
+import java.io.*;
+import java.text.*;
+import java.util.*;
+import java.util.Timer;
+
+public class ShopMenu {
+    JTextArea display;
+
+    ShopMenu() {
+        JFrame f = new JFrame("Shop");
+        JPanel panel = new JPanel();
+        panel.setBorder(new TitledBorder(new EtchedBorder(), "Shop Time!"));
+        SpringLayout layout = new SpringLayout();
+
+        JButton moneyMult = new JButton("Money multiplier");
+
+        moneyMult.addActionListener(new ActionListener() {
+            private PrintStream display;
+
+            public void actionPerformed(ActionEvent a) {
+                System.out.println("Money Multipler");
+                display.append("Money Multipler");
+            }
+        });
+
+        panel.add(moneyMult);
+
+        f.add(panel);
+
+        panel.setBackground(new Color(25, 30, 36));
+
+        panel.setForeground(new Color(191, 198, 204));
+
+        f.setResizable(false);
+        f.pack();
+        f.setSize(300, 200);
+        f.setLocationRelativeTo(null); // Centers JFrame
+        f.getContentPane().setBackground(Color.BLACK);
+        f.add(panel);
+        f.setVisible(true);
+    }
+}
