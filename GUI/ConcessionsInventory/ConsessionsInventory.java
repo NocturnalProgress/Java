e98e72bf86775a412b17a3a375c1079ac2a691ad
NocturnalProgress <26844071+NocturnalProgress@users.noreply.github.com>
2020-04-01 14:22:42 -0400

    Created concessions inventory main class

---
+++ GUI/ConcessionsInventory/ConsessionsInventory.java
@@ -0,0 +1,13 @@
+package GUI.ConcessionsInventory;
+
+import javax.swing.*;
+import javax.swing.border.*;
+import java.awt.event.*;
+import java.awt.Color;
+import java.io.*;
+
+class ConcessionsInventory {
+    public static void main(String[] args) throws IOException {
+        new ConInvGUI();
+    }
+}
