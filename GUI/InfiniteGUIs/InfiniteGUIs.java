9c18771b22eeb0efd2c99c713058d0e5f0b329bf
NocturnalProgress <26844071+NocturnalProgress@users.noreply.github.com>
2020-04-01 14:24:29 -0400

    Created InfiniteGUIs.java

---
+++ GUI/InfiniteGUIs/InfiniteGUIs.java
@@ -0,0 +1,51 @@
+package GUI.InfiniteGUIs;
+
+import javax.swing.*;
+import javax.swing.border.*;
+import java.awt.event.*;
+import java.awt.Color;
+import java.io.*;
+import java.awt.*;
+import java.util.List;
+import java.util.*;
+
+class InfiniteGUIs {
+    boolean loop = true;
+    Random rand = new Random();
+    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    int height = screenSize.height;
+    int width = screenSize.width;
+
+    InfiniteGUIs() {
+
+        List<JFrame> frameList = new ArrayList<JFrame>();
+        while (loop = true) {
+            try {
+                Thread.sleep(100);
+                JFrame frameLoop = new JFrame("Get Pranked!");
+                JPanel panelLoop = new JPanel();
+
+                int size1 = rand.nextInt(height);
+                int size2 = rand.nextInt(width);
+
+                int randColor1 = rand.nextInt(255);
+                int randColor2 = rand.nextInt(255);
+                int randColor3 = rand.nextInt(255);
+
+                frameLoop.add(panelLoop);
+                panelLoop.setBackground(new Color(randColor1, randColor2, randColor3));
+                frameLoop.setVisible(true);
+                frameLoop.setSize(size2, size1);
+                frameLoop.setLocationRelativeTo(null);
+
+            } catch (InterruptedException ie) {
+                ie.printStackTrace();
+            }
+
+        }
+    }
+
+    public static void main(String[] args) {
+        new InfiniteGUIs();
+    }
+}
