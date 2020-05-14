0e9d796e4d43cd9260bda8ad8c51a6844b8d45bc
NocturnalProgress <26844071+NocturnalProgress@users.noreply.github.com>
2020-05-14 17:45:27 -0400

    Removed second text generator

--- GUI/MegaGUI/RandomTextGen.java
+++ GUI/MegaGUI/RandomTextGen.java
@@ -21,18 +21,8 @@ public class RandomTextGen {
         JTextField displayRandText = new JTextField("", +16);
         displayRandText.setHorizontalAlignment(JTextField.CENTER); // Center text
 
-        JButton NSFWrandText = new JButton("NSFW Random Name Generator");
-        JTextField displayNSFWRandText = new JTextField("", +16);
-        displayNSFWRandText.setHorizontalAlignment(JTextField.CENTER); // Center text
-
         randText.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent a) {
-                counter = counter + 1;
-                if (counter == 20) {
-                    panel.add(NSFWrandText);
-                    panel.add(displayNSFWRandText);
-                    panel.revalidate();
-                }
 
                 List<String> list = Arrays.asList("Daraxus", "Chromblip", "Zarth", "Crebis", "Farlip", "Thorndip",
                         "Prenus", "Grublim", "Dinglegorf", "Sarcobious", "Blumpid", "DingleSchwartz", "Arcondus",
@@ -43,20 +33,6 @@ public class RandomTextGen {
                 String randomElement = list.get(randomitem);
                 displayRandText.setText(randomElement);
 
-            }
-        });
-
-        NSFWrandText.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent b) {
-                List<String> list = Arrays.asList("Haywood Jablomi", "Mike Oxlong", "Strockinoff", "Gabe Itch",
-                        "Tess Tickles", "Holden Tudiks", "Ben Dover", "Anita Cock", "Mike Hawk", "Jenny Tulls",
-                        "Craven Morehead", "Dixie Nourmous", "Mike Litorus", "Barry Mckockiner", "Jack MeHoff",
-                        "Benjamin Dover");
-
-                Random randInt = new Random();
-                int randomitem = randInt.nextInt(list.size());
-                String randomElement = list.get(randomitem);
-                displayNSFWRandText.setText(randomElement);
             }
         });
 
@@ -72,11 +48,6 @@ public class RandomTextGen {
         layout.putConstraint(SpringLayout.NORTH, randText, 5, SpringLayout.NORTH, panel);
         layout.putConstraint(SpringLayout.WEST, displayRandText, -178, SpringLayout.EAST, randText);
         layout.putConstraint(SpringLayout.NORTH, displayRandText, 50, SpringLayout.NORTH, panel);
-
-        layout.putConstraint(SpringLayout.WEST, NSFWrandText, 85, SpringLayout.WEST, panel);
-        layout.putConstraint(SpringLayout.NORTH, NSFWrandText, 100, SpringLayout.NORTH, panel);
-        layout.putConstraint(SpringLayout.WEST, displayNSFWRandText, -200, SpringLayout.EAST, NSFWrandText);
-        layout.putConstraint(SpringLayout.NORTH, displayNSFWRandText, 150, SpringLayout.NORTH, panel);
 
         f.add(panel);
         f.pack();
